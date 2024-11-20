ffa98b5da11abbd13f3e65ec803178559a4784e8
Emil Öhrström <Emil.ohrstrom@hotmail.com>
2024-11-20 15:44:00 +0100

    Det är bra om man lägger till allt innan commit

--- BudgetBuddy.Blazor/Models/Category.cs
+++ BudgetBuddy.Blazor/Models/Category.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BudgetBuddy.Models;
 
 public class Category
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Kategorinamn är obligatoriskt.")]
     public string Name { get; set; }
+    
+    public decimal BudgetLimit { get; set; }
 }
--- BudgetBuddy.Blazor/Services/BudgetService.cs
+++ BudgetBuddy.Blazor/Services/BudgetService.cs
@@ -1,6 +1,23 @@
+using BudgetBuddy.Models;
+
 namespace BudgetBuddy.Services;
 
 public class BudgetService
 {
+    private readonly List<BudgetItem> _budgetItems = new();
+    private readonly List<Category> _categories = new();
+    public List<BudgetItem> GetBudgetItems() => _budgetItems;
+
+    public void AddBudgetItem(BudgetItem budgetItem)
+    {
+        _budgetItems.Add(budgetItem);
+    }
     
+    public List<Category> GetCategories() => _categories;
+
+    public void AddCategory(Category category)
+    {
+        Console.WriteLine($"Adding Category: Name={category.Name}, Limit={category.BudgetLimit}");
+        _categories.Add(category);
+    }
 }
