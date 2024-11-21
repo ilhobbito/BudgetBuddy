992a489314c6be5d38e49ec8e1e8ee7fbed441fd
Tim Tallberg <tim.tallis@hotmail.com>
2024-11-21 15:43:31 +0100

    Made some basic controller function for BudgetItemController

---
+++ BudgetBuddy.Api/Controllers/BudgetItemsControllers.cs
@@ -0,0 +1,35 @@
+using BudgetBuddy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class BudgetItemsController : Controller
+{
+    private readonly BudgetBuddyContext _context;
+
+    public BudgetItemsController(BudgetBuddyContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public void CreateBudgetItem([FromBody]BudgetItem budgetItem)
+    {
+        
+        _context.BudgetItems.Add(budgetItem);
+        _context.SaveChanges();
+
+        
+    }
+
+    [HttpGet]
+    public IActionResult GetBudgetItems()
+    {
+        var budgetItems = _context.BudgetItems.ToList();
+        return Ok(budgetItems);
+    }
+}
--- BudgetBuddy.Lib/Models/BudgetItem.cs
+++ BudgetBuddy.Lib/Models/BudgetItem.cs
@@ -1,18 +1,27 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace BudgetBuddy.Models;
 
 public class BudgetItem
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
     
+    [JsonPropertyName("name")]
     [Required(ErrorMessage = "Inkomstkälla är obligatoriskt.")]
     public string Name { get; set; }
     
+    [JsonPropertyName("amount")]
     [Range(1, double.MaxValue, ErrorMessage = "Beloppet måste vara större än 0.")]
     public decimal Amount { get; set; }
     
-    public int CategoryId { get; set; } 
+    [JsonPropertyName("categoryId")]
+    public int CategoryId { get; set; }
+    
+    [JsonPropertyName("date")]
     public DateTime Date { get; set; } = DateTime.Now;
+    
+    [JsonPropertyName("isIncome")]
     public bool IsIncome { get; set; }
 }
