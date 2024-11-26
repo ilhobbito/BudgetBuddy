1eea1b112f6f97c8ef769e7e7fc692113ec910b5
timta@Tallis <tim.tallis@hotmail.com>
2024-11-26 09:22:40 +0100

    Started working in DAL

--- BudgetBuddy.Lib/DAL/BudgetItemManager.cs
+++ BudgetBuddy.Lib/DAL/BudgetItemManager.cs
@@ -1,6 +1,44 @@
+using System.Text.Json;
+using BudgetBuddy.Models;
+
 namespace BudgetBuddy.Lib.DAL;
 
 public class BudgetItemManager
 {
+    private static readonly Uri BaseAddress = new Uri("https://localhost:44346/");
     
+    public async Task<List<BudgetItem>> GetBudgetItemsAsync()
+    {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = BaseAddress;
+            HttpResponseMessage response = await client.GetAsync("api/BudgetItems");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                List<BudgetItem> budgetItems = JsonSerializer.Deserialize<List<BudgetItem>>(responseString);
+                return budgetItems;
+            }
+
+            return new List<BudgetItem>();
+        }
+    }
+
+    public async Task<BudgetItem> GetBudgetItemByIdAsync(int id)
+    {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = BaseAddress;
+            HttpResponseMessage response = await client.GetAsync("api/BudgetItems");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                BudgetItem budgetItem = JsonSerializer.Deserialize<BudgetItem>(responseString);
+                return budgetItem;
+            }
+
+            return null;
+    }
 }
