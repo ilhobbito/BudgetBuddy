e20e59d77440d7a8f3b8f6ff5a0165d6dbc20627
Tim Tallberg <tim.tallis@hotmail.com>
2024-11-26 15:41:15 +0100

    Undid the previous change to see if it works as intented now

--- Budgetbuddy.tests/BudgetItemManagerTests.cs
+++ Budgetbuddy.tests/BudgetItemManagerTests.cs
@@ -44,35 +44,7 @@ public class BudgetItemManagerTests
         // Assert
         Assert.Equal(expectedResponse.StatusCode, actualResponse.StatusCode);
     }
-    [Fact]
-    public async Task TestIfCanCreateBudgetItemAgain()
-    {
-      
-        var mockHttpMessageHandler = new MockHttpMessageHandler((request, cancellationToken) =>
-        {
-            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
-        });
-        
-        var client = new HttpClient(mockHttpMessageHandler);
-        var sutBudgetItemManager = new BudgetItemManager(client);
-        
-        // Arrange Added this to push something new
-        var budgetItem = new BudgetItem()
-        {
-            Name = "Testitem",
-            CategoryId = 1,
-            Amount = 200m,
-            IsIncome = true,
-            Date = DateTime.Now,
-        };
-        
-        HttpResponseMessage expectedResponse = new HttpResponseMessage(System.Net.HttpStatusCode.Conflict); 
-
-        // Act
-        var actualResponse = await sutBudgetItemManager.CreateBudgetItemAsync(budgetItem);
-        // Assert
-        Assert.Equal(expectedResponse.StatusCode, actualResponse.StatusCode);
-    }
+    
 
    
 }
