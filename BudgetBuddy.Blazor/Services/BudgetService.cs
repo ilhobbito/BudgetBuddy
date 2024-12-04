20aabe25ee1fb2d3e80a35fadbc3c8e187ad4dab
Emiltjalol <Emil.ohrstrom@hotmail.com>
2024-12-04 11:42:09 +0100

    fixed some bugs

--- BudgetBuddy.Blazor/Services/BudgetService.cs
+++ BudgetBuddy.Blazor/Services/BudgetService.cs
@@ -17,25 +17,27 @@ public class BudgetService
     public static List<BudgetItem> BudgetItems { get; set; }
     public async Task SeedCategoriesAndBudgetItemsAsync(List<Category> categories, List<BudgetItem> budgetItems)
     {
-        if (budgetItems.Count() > 0)
+        
+        if (categories.Count() <= 0)
+        {
+            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Inkomst"});
+            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Mat", BudgetLimit = 5000});
+            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Nöjen", BudgetLimit = 2500});
+            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Kläder", BudgetLimit = 1500});
+            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Övrigt", BudgetLimit = 1000});
+        }
+        if (budgetItems.Count() <= 0)
         {
             await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Lön", Amount = 2000, IsIncome = true, CategoryId = 1 });
-            await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "OF", Amount = 1337, IsIncome = true, CategoryId = 1});
-            await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Twitch", Amount = 99, IsIncome = true, CategoryId = 1});
+            await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Studiebidrag", Amount = 1337, IsIncome = true, CategoryId = 1});
+            await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Twitch streaming", Amount = 99, IsIncome = true, CategoryId = 1});
             await  _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Barnbidrag", Amount = 2400, IsIncome = true, CategoryId = 1});
             await  _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Netflix", Amount = 149, IsIncome = false, CategoryId = 3 });
-            await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Pornhub subscription", Amount = 99, IsIncome = false, CategoryId = 3 });
+            await _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Disney plus", Amount = 99, IsIncome = false, CategoryId = 3 });
             await  _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "Donken", Amount = 99, IsIncome = false, CategoryId = 2 });
             await  _budgetItemManager.CreateBudgetItemAsync(new BudgetItem() { Name = "H&M", Amount = 499, IsIncome = false, CategoryId = 4 });
         }
-        if (categories.Count > 0)
-        {
-            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Inkomst",  Id = 1 });
-            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Mat", BudgetLimit = 5000, Id = 2 });
-            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Nöjen", BudgetLimit = 2500, Id = 3 });
-            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Kläder", BudgetLimit = 1500, Id = 4 });
-            await  _categoriesManager.CreateCategoryAsync(new Category() { Name = "Övrigt", BudgetLimit = 1000, Id = 5 });
-        }
+      
     }
 
     public async Task AddBudgetItem(BudgetItem budgetItem)
@@ -57,19 +59,19 @@ public class BudgetService
         var totalExpenses = await GetTotalAmount(false);
         return totalIncome - totalExpenses;
     }
-    
 
-    public async Task<string> AddCategory(Category category)
-    {
-        await _categoriesManager.CreateCategoryAsync(category);
-        // Check where the messages pops out and see if it's necessary.
-        return category.Name != "" ? category.Name : "Okänd kategori";
-    }
+
+    // public async Task<string> AddCategory(Category category)
+    // {
+    //     await _categoriesManager.CreateCategoryAsync(category);
+    //     // Check where the messages pops out and see if it's necessary.
+    //     return category.Name != "" ? category.Name : "Okänd kategori";
+    // }
     
-    public async Task<string> GetCategoryName(int categoryId)
-    {
-        var category = await _categoriesManager.GetCategoryByIdAsync(categoryId);
-        return category != null ? category.Name : "Okänd kategori";
-    }
+    // public async Task<string> GetCategoryName(int categoryId)
+    // {
+    //     var category = await _categoriesManager.GetCategoryByIdAsync(categoryId);
+    //     return category != null ? category.Name : "Okänd kategori";
+    // }
     
 }
